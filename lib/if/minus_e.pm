02690d5d180358b70ee28c923a7d237ecaf27ad3
Matt S Trout <mst@shadowcat.co.uk>
2013-05-23 17:18:28 +0000

    Document if::minus_e

--- lib/if/minus_e.pm
+++ lib/if/minus_e.pm
@@ -13,3 +13,47 @@ sub import {
 }
 
 1;
+
+=head1 NAME
+
+if::minus_e - only load and import modules for one-liners
+
+=head1 SYNOPSIS
+
+This:
+
+  $ perl -Mif::minus_e=Some::Module myscript.pl
+
+is equivalent to just:
+
+  $ perl myscript.pl
+
+However, this:
+
+  $ perl -Mif::minus_e=Some::Module -e '...'
+
+is equivalent to:
+
+  $ perl -MSome::Module -e '...'
+
+or, similarly:
+
+  $ perl -e 'use Some::Module; ...'
+
+And this:
+
+  $ perl -Mif::minus_e=Some::Module,foo,bar -e '...'
+
+is equivalent to:
+
+  $ perl -MSome::Module=foo,bar -e '...'
+
+or, again:
+
+  $ perl -e 'use Some::Module qw(foo bar); ...'
+
+=head1 AUTHOR and COPYRIGHT
+
+See L<App::MyPerl>
+
+=cut
